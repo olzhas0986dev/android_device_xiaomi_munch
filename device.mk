685ab5c7f9ebd9beb7489d21afa723bb3ffe8090
olzhas0986dev <apohuj789@gmail.com>
2025-06-18 12:17:10 +0500

    munch: Add support for ViPER4Android FX and build it

--- device.mk
+++ device.mk
@@ -19,6 +19,9 @@ ifeq ($(TARGET_USES_DOLBY),true)
 $(call inherit-product, hardware/dolby/dolby.mk)
 endif
 
+# ViPER4AndroidFX
+$(call inherit-product, packages/apps/ViPER4AndroidFX/config.mk)
+
 # Private key for signed build
 -include vendor/private-keys/keys/keys.mk
 
